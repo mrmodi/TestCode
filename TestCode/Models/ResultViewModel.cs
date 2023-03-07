9b89afa3ad613b49f13019d49da3899acc442b46
mrmodi <manan211197@gmail.com>
2023-03-06 23:48:16 -0500

    date - 06/03/20203 modifiedby manan

--- TestCode/Models/ResultViewModel.cs
+++ TestCode/Models/ResultViewModel.cs
@@ -1,9 +1,18 @@
-﻿namespace TestCode.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TestCode.Models
 {
     public class ResultViewModel
     {
         public string ExtractedText { get; set; }
         public List<string> Values { get; set; }
         public List<int> Counts { get; set; }
+      
+        public string FilePath { get; set; }
+       
+        public double AverageConfidence { get; internal set; }
+        public long FileSize { get; internal set; }
+        public string FormattedFileSize { get; internal set; }
+        public string SelectedValue { get; internal set; }
     }
 }
---
+++ TestCode/Models/TextAndConfidence.cs
@@ -0,0 +1,8 @@
+﻿namespace TestCode.Models
+{
+    public class TextAndConfidence
+    {
+        /*public string Text { get; set; }
+        public double Confidence { get; set; }*/
+    }
+}
